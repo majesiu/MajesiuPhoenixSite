5a0ac8af597ecc9151b5c3dc7038419275f3d2ee
Pawel Stefan Majewski <pawel.j.majewski@student.put.poznan.pl>
2017-04-03 23:53:13 +0200

    Added Page about my technologies

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule MajesiuSite.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def techs(conn, _params) do
+    render conn, "techs.html"
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule MajesiuSite.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/techs", PageController, :techs
   end
 
   # Other scopes may use custom stacks.
